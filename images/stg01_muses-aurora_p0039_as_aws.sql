03fd63cd5095cf38522ac7770b427885bc846d0b
scott.hsieh[謝書正] <scott.hsieh@scotthsieh-mac13.local>
2022-06-12 13:15:03 +0800

    upgrade projen

--- images/stg01_muses-aurora_p0039_as_aws.sql
+++
@@ -1,8 +0,0 @@
--- DMS CDC required permission
-GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'all_p0039_as_aws'@'10.128.8.0/255.255.248.0' IDENTIFIED BY 'all_p0039_as_aws';
-
--- For RDS named 'muses-aurora' in the AWS account named stg01
-GRANT SELECT ON `muses`.* TO 'all_p0039_as_aws'@'10.128.8.0/255.255.248.0';
-GRANT SELECT ON `ai_interview_tool`.* TO 'all_p0039_as_aws'@'10.128.8.0/255.255.248.0';
-GRANT SELECT ON `highschool_ai_interview`.* TO 'all_p0039_as_aws'@'10.128.8.0/255.255.248.0';
-GRANT SELECT ON `sprint_ai_interview`.* TO 'all_p0039_as_aws'@'10.128.8.0/255.255.248.0';
